154e86d43688ae7a2eba3e46c242a0834245adc8
junho lee <junho1273@gmail.com>
2022-08-13 23:18:29 +0900

    유저가 나갔을때 접속유저 리스트 제거

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ let user = []
 let CountArr = []
 let RoomCount = 1
 io.on('connection',function(socket){
-  //유저 데이터 받고 유저 목록 보내기
   socket.on('nickname',(data)=>{
     user.push(
       {
@@ -41,18 +40,36 @@ io.on('connection',function(socket){
     }else{
       CountArr.push(RoomCount)
     }
-    socket.join(`room ${RoomCount} `)
+    socket.join(`room${RoomCount}`)
     //유저 닉네임과 방 생성 id가 일치하는 이름만 빼기(방 생성자를 저장하기 위한..)    
     let userName  = user.filter(e=>{
       if(e.id === socket.id) return e.nickname  
     })
     //방 정보 사용자에게 내보내기
     let roomData ={
-      'title' : `room ${RoomCount}`,
+      'title' : `room${RoomCount}`,
       'player' : userName[0] ? userName[0].nickname : '너이름안정해써'
     }
     roomDataArr.push(roomData)
     io.emit('createRoom',roomData)
   })
-    roomDataArr[0] && io.to(socket.id).emit('roomData',[roomDataArr,user])
+  //소켓을 나가면 유저 데이터를 없애줌
+  socket.on('babo',(data)=>{
+    console.log(data)
+  })
+  socket.on("disconnect", (reason) => {
+    //유저가 나가면 나간 유저 배열 뒤져서 삭제하고 나간 유저이름
+     user.forEach((e,i)=>{
+      if(e.id === socket.id){
+        user.splice(i,1)
+        io.emit('disconnectUser',e.nickname)
+      }
+     })
+     
+  });
+  socket.on('joinRoom',(data)=>{
+    socket.join(data)
+    console.log(socket.id,`${data}접속`)
+  })
+    roomDataArr[0] && io.to(socket.id).emit('roomsData',[roomDataArr,user])
 })
